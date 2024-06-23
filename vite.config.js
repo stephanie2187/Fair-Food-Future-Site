ac7c5abb659e19b4047c7bde1b8fb656187036c6
Stephanie Lim <2005nini@gmail.com>
2024-06-23 16:38:43 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['framer-motion'],
+      external: ['framer-motion', 'react-icons/bs'],
     },
   },
 });
